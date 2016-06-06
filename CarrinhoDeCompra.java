fa7e6559cf58fb7bed704c4b1905807947924902
waneves <tomcatanduva@gmail.com>
2016-06-06 10:58:42 -0300

    Atividade Testada

---
+++ CarrinhoDeCompra.java
@@ -0,0 +1,39 @@
+import java.util.ArrayList;
+
+/**
+ * Created by wellingtonalvesdasneves on 6/6/16.
+ */
+public class CarrinhoDeCompra {
+
+    private ArrayList<Pizza> itensCarrinho = new ArrayList<Pizza>();
+    private double vrTotal = 0;
+
+    public void addPizza(Pizza pizza){
+        if (pizza.totalIngredientes > 0) {
+            this.itensCarrinho.add(pizza);
+        }
+    }
+
+    public double getVrTotal() {
+
+        for (Pizza p: itensCarrinho) {
+            vrTotal += p.getPreco();
+        }
+
+        return vrTotal;
+    }
+
+    public void listaSabores(){
+
+        for (Pizza p: itensCarrinho) {
+            System.out.println(" * " + p.getSabor());
+        }
+        System.out.println();
+    }
+
+    public void listaIngredientesPizza(){
+        for( String p : Pizza.ingredientes.keySet() ){
+            System.out.println("Ingrediente: " + p + " - Qtde: " + Pizza.ingredientes.get(p) );
+        }
+    }
+}
---
+++ Principal.java
@@ -0,0 +1,45 @@
+import javax.sound.midi.Soundbank;
+
+/**
+ * Created by wellingtonalvesdasneves on 6/6/16.
+ */
+public class Principal {
+    public static void main(String[] args) {
+        Pizza p1 = new Pizza("Calabresa");
+        Pizza p2 = new Pizza("Presunto e Queijo");
+        Pizza p3 = new Pizza("Frango com Catupiry");
+
+        p1.adicionaIngrediente("Molho de Tomate");
+        p1.adicionaIngrediente("Calabresa");
+        p1.adicionaIngrediente("Massa");
+
+        p2.adicionaIngrediente("Presunto");
+        p2.adicionaIngrediente("Mussarela");
+        p2.adicionaIngrediente("Molho de Tomate");
+        p2.adicionaIngrediente("Massa");
+
+        p3.adicionaIngrediente("Frango");
+        p3.adicionaIngrediente("Catupiry");
+        p3.adicionaIngrediente("Massa");
+
+        CarrinhoDeCompra carrinhoDeCompra = new CarrinhoDeCompra();
+
+        carrinhoDeCompra.addPizza(p1);
+        carrinhoDeCompra.addPizza(p2);
+        carrinhoDeCompra.addPizza(p3);
+
+        System.out.println("| Relatório de Sabores |");
+        carrinhoDeCompra.listaSabores();
+
+        System.out.println("| Relatório de Valor |");
+        if (carrinhoDeCompra.getVrTotal() > 0 ) {
+            System.out.println("Preço Total: R$ " + carrinhoDeCompra.getVrTotal());
+        }else {
+            System.out.println("Carrinho de Compras Vazio!");
+        }
+
+        System.out.println();
+        System.out.println("| Relatório de Ingredientes |");
+        carrinhoDeCompra.listaIngredientesPizza();
+    }
+}
